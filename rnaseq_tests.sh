a9699809fe2ca2c17f70483cbcce970714207f2d
Alexander Peltzer <alex.peltzer@gmail.com>
2019-03-21 12:54:23 +0100

    Calls to respective scripts

--- rnaseq_tests.sh
+++ rnaseq_tests.sh
@@ -3,6 +3,7 @@
 RELEASE=$1
 
 ##Run No1 of the integration test samples
-nextflow run nf-core/rnaseq -profile cfc -r ${RELEASE} --genome 'GRCh37' --reads '' 
+nextflow run nf-core/rnaseq -profile cfc -r ${RELEASE} --genome 'GRCm38' --reads 'rnaseq/mouse/*.fastq.gz' --singleEnd
 
 ##Run No2 of the integration test samples
+nextflow run nf-core/rnaseq -profile cfc -r ${RELEASE} --genome 'GRCh37' --reads 'rnaseq/human/*R{1,2}*fastq.gz'
