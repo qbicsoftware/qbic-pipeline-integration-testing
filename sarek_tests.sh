bffda200ef3933f71658aa32cf47e1295eaa1ca4
Alexander Peltzer <alex.peltzer@gmail.com>
2019-03-27 15:21:49 +0100

    Add proper outDir

--- sarek_tests.sh
+++ sarek_tests.sh
@@ -12,18 +12,18 @@ RELEASE=$1
 
 ##Run No1 of the integration test samples (WES data, somatic and germline)
 #Run Mapping first
-nextflow run scilifelab/Sarek/main.nf -profile cfc -r ${RELEASE} --genome 'GRCh37' --sample 'sarek-testsets/wes_input.tsv' --outdir "results/sarek/wes"
+nextflow run scilifelab/Sarek/main.nf -profile cfc -r ${RELEASE} --genome 'GRCh37' --sample 'sarek-testsets/wes_input.tsv' --outDir "results/sarek/wes"
 
 #Run VC on germline data next
-nextflow run scilifelab/Sarek/germlineVC.nf -profile cfc -r ${RELEASE} --genome 'GRCh37' --sample 'results/sarek/wes/Preprocessing/Recalibrated/recalibrated.tsv' --tools 'HaplotypeCaller,strelka,manta' --targetBED 'sarek-testsets/testcap.bed' --outdir "results/sarek/wes"
+nextflow run scilifelab/Sarek/germlineVC.nf -profile cfc -r ${RELEASE} --genome 'GRCh37' --sample 'results/sarek/wes/Preprocessing/Recalibrated/recalibrated.tsv' --tools 'HaplotypeCaller,strelka,manta' --targetBED 'sarek-testsets/testcap.bed' --outDir "results/sarek/wes"
 
 #Run VC on somatic data next
-nextflow run scilifelab/Sarek/somaticVC.nf -profile cfc -r ${RELEASE} --genome 'GRCh37' --sample 'results/sarek/wes/Preprocessing/Recalibrated/recalibrated.tsv'  --tools manta,strelka,ascat,mutect2 --targetBED 'sarek-testsets/testcap.bed'
+nextflow run scilifelab/Sarek/somaticVC.nf -profile cfc -r ${RELEASE} --genome 'GRCh37' --sample 'results/sarek/wes/Preprocessing/Recalibrated/recalibrated.tsv' --tools manta,strelka,ascat,mutect2 --targetBED 'sarek-testsets/testcap.bed' --outDir "results/sarek/wes"
 
 #Annotate this stuff
-nextflow run scilifelab/Sarek/annotate.nf -profile cfc -r ${RELEASE} --genome 'GRCh37' --annotateTools 'HaplotypeCaller,strelka,manta' --tools 'VEP,snpEff' --outdir "results/sarek/wes"
+nextflow run scilifelab/Sarek/annotate.nf -profile cfc -r ${RELEASE} --genome 'GRCh37' --annotateTools 'HaplotypeCaller,strelka,manta' --tools 'VEP,snpEff'--outDir "results/sarek/wes"
 
 #Run MultiQC on the stuff
-nextflow run scilifelab/Sarek/runMultiQC.nf -profile cfc -r ${RELEASE} --outdir "results/sarek/wes"
+nextflow run scilifelab/Sarek/runMultiQC.nf -profile cfc -r ${RELEASE} --outDir "results/sarek/wes"
 
 
