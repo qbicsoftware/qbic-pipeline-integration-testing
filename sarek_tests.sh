173b568c6f7f4746c312a9edc0f008d9bce31c6d
Alexander Peltzer <alex.peltzer@gmail.com>
2019-03-27 14:48:34 +0100

    Update sarek tests for multiple changes

--- sarek_tests.sh
+++ sarek_tests.sh
@@ -13,9 +13,12 @@ RELEASE=$1
 ##Run No1 of the integration test samples
 #Run Mapping first
 nextflow run scilifelab/Sarek/main.nf -profile cfc -r ${RELEASE} --sample 'sarek-testsets/wes_input.tsv' --genome 'GRCh37' --outdir "results/sarek/wes"
-#Run VC next
+
+#Run VC on germline data next
 nextflow run scilifelab/Sarek/germlineVC.nf -r ${RELEASE} --sample 'results/sarek/wes/Preprocessing/Recalibrated/recalibrated.tsv' -profile cfc --genome 'GRCh37' --tools 'HaplotypeCaller,strelka,manta' --targetBED 'sarek-testsets/testcap.bed' --outdir "results/sarek/wes"
-#Somatic stuff missing here
+
+#Run VC on somatic data next
+nextflow run scilifelab/Sarek/somaticVC.nf -r ${RELEASE} --sample 'results/sarek/wes/Preprocessing/Recalibrated/recalibrated.tsv' -profile cfc --genome 'GRCh37' --tools manta,strelka,ascat,mutect2 --targetBED 'sarek-testsets/testcap.bed'
 
 #Annotate this stuff
 nextflow run scilifelab/Sarek/annotate.nf -r ${RELEASE} -profile cfc --genome 'GRCh37' --annotateTools 'HaplotypeCaller,strelka,manta' --tools 'VEP,snpEff' --outdir "results/sarek/wes"
