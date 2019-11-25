9e8d36b9d5f5c51f23a6b32075e2aacc571ec688
Alexander Peltzer <apeltzer@users.noreply.github.com>
2019-11-25 14:29:14 +0100

    Update datastager.sh

--- datastager.sh
+++ datastager.sh
@@ -4,7 +4,7 @@
 ###Load required modules 
 module purge 
 module load qbic/qpostman/0.3.0
-module load qbic/singularity_slurm/3.0.3
+module load devel/singularity/3.4.2
 module load devel/java_jdk/1.8.0u121
 
 ###Stage Data now
